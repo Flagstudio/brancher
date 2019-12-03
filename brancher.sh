a3543d71fa7a187ed93f0a57771e48a35e786845
vasishakd <vasishak.d@gmail.com>
2019-12-03 11:05:07 +0500

    Update brancher.sh

--- brancher.sh
+++ brancher.sh
@@ -3,71 +3,73 @@
 # Brancher
 # https://github.com/Flagstudio/brancher
 
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-NC='\033[0m' # No Color
+function brancher() {
+    RED='\033[1;31m'
+    GREEN='\033[1;32m'
+    NC='\033[0m' # No Color
 
-COLUMNS=12  
-ENV_FILE=".env"
-REQUIRED_APP_ENV="trash"
+    COLUMNS=12  
+    ENV_FILE=".env"
+    REQUIRED_APP_ENV="trash"
 
-# Check previous command error status
-check_command_exec_status () {
-  if [[ $1 -eq 0 ]]
-    then
-      echo -e "${YELLOW}Success!${NC}"
-      echo
-  else
-    echo -e "${L_RED}ERROR${NC}"
-    echo
-  fi
-}
+    # Check previous command error status
+    check_command_exec_status () {
+      if [[ $1 -eq 0 ]]
+        then
+          echo -e "${YELLOW}Success!${NC}"
+          echo
+      else
+        echo -e "${L_RED}ERROR${NC}"
+        echo
+      fi
+    }
 
-echo -e "Looking for ${ENV_FILE} in current directory..."
-if [ ! -f "$ENV_FILE" ]; then
-    echo -e "${RED}${ENV_FILE} not found${NC}"
-    return
-fi
-
-echo -e "Sourcing ${ENV_FILE} file..."
-source "$ENV_FILE"
-check_command_exec_status $?
-
-if [ "$APP_ENV" != "$REQUIRED_APP_ENV" ]; then
-    echo -e "${RED}APP_ENV in ${ENV_FILE} should be ${REQUIRED_APP_ENV}. Current APP_ENV is ${APP_ENV}${NC}"
-    return
-fi
-
-echo -e "Fetching data from remote..."
-git fetch
-check_command_exec_status $?
-
-branches=()
-eval "$(git for-each-ref --shell --format='branches+=(%(refname:strip=3))' refs/remotes/)"
-branches+=('Exit')
-
-PS3='Enter your choice: '
-select branch in "${branches[@]}"
-do
-    if [ $REPLY -eq ${#branches[@]} ]; then
-        break
+    echo -e "Looking for ${ENV_FILE} in current directory..."
+    if [ ! -f "$ENV_FILE" ]; then
+        echo -e "${RED}${ENV_FILE} not found${NC}"
+        return
     fi
-    if [ $REPLY -gt ${#branches[@]} ] || [ $REPLY -lt 1 ]; then
-        echo -e "${RED}invalid number${NC}"
-    else
-        git stash
-        git checkout $branch
-        if [ $? -eq 0 ]; then
-            echo -e "Working like a nigger..."
-            git reset --hard origin/$branch
-            composer install --quiet
-            php artisan migrate:fresh --seed
-            npm run --silent prod
-            pa view:clear
-            echo -e "${GREEN}branch changed${NC}"
+
+    echo -e "Sourcing ${ENV_FILE} file..."
+    source "$ENV_FILE"
+    check_command_exec_status $?
+
+    if [ "$APP_ENV" != "$REQUIRED_APP_ENV" ]; then
+        echo -e "${RED}APP_ENV in ${ENV_FILE} should be ${REQUIRED_APP_ENV}. Current APP_ENV is ${APP_ENV}${NC}"
+        return
+    fi
+
+    echo -e "Fetching data from remote..."
+    git fetch
+    check_command_exec_status $?
+
+    branches=()
+    eval "$(git for-each-ref --shell --format='branches+=(%(refname:strip=3))' refs/remotes/)"
+    branches+=('Exit')
+
+    PS3='Enter your choice: '
+    select branch in "${branches[@]}"
+    do
+        if [ $REPLY -eq ${#branches[@]} ]; then
             break
-        else
-            echo -e "${RED}fail${NC}"
         fi
-    fi
-done
+        if [ $REPLY -gt ${#branches[@]} ] || [ $REPLY -lt 1 ]; then
+            echo -e "${RED}invalid number${NC}"
+        else
+            git stash
+            git checkout $branch
+            if [ $? -eq 0 ]; then
+                echo -e "Working like a nigger..."
+                git reset --hard origin/$branch
+                composer install --quiet
+                php artisan migrate:fresh --seed
+                npm run --silent prod
+                pa view:clear
+                echo -e "${GREEN}branch changed${NC}"
+                break
+            else
+                echo -e "${RED}fail${NC}"
+            fi
+        fi
+    done
+}
